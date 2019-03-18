68cc37754258eaeae4b1f0d15ec02a26a9966d18
Sasha Sushko <sushko@outlook.com>
2019-03-19 00:02:25 +0500

    Использует ранее созданные компоненты

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React, { Component } from 'react';
-import TopBar from './components/TopBar';
-import SideBar from './components/SideBar';
-import MainContent from './components/MainContent';
-import ServiceList from './components/ServiceList';
-import Footer from './components/Footer';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Container,
+  TopBar,
+  SideBar,
+  Main,
+  Footer
+} from "./components/Layout/Layout";
+import Balance from "./components/Balance/Balance";
+import Nav from "./components/Nav/Nav";
+import Page from "./components/Page/Page";
+import ServiceList from "./components/ServiceList";
+import Copyright from "./components/Copyright/Copyright";
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <TopBar />
-        <SideBar />
-        <MainContent title="Сервисы">
-          <ServiceList />
-        </MainContent>
-        <Footer />
-      </React.Fragment>
+      <Container>
+        <SideBar>
+          <Nav />
+        </SideBar>
+        <TopBar>
+          <Balance />
+        </TopBar>
+        <Main>
+          <Page title="Сервисы">
+            <ServiceList />
+          </Page>
+        </Main>
+        <Footer>
+          <Copyright />
+        </Footer>
+      </Container>
     );
   }
 }
