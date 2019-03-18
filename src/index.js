72b74968276bed9f485d666842ddb38c83c7cbfd
Sasha Sushko <sushko@outlook.com>
2019-03-19 00:05:26 +0500

    Импортирует только нужный метод

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import ReactDOM from 'react-dom';
 import Api from './api/api';
 import { ApiProvider } from './api/api-injection';
 import App from './App';
 import './index.css';
 import * as React from "react";
+import { render } from "react-dom";
 
 const api = new Api();
 
-ReactDOM.render(
-    <ApiProvider value={api}>
-        <App />
-    </ApiProvider>,
-    document.getElementById('root')
+render(
+  <ApiProvider value={api}>
+    <App />
+  </ApiProvider>,
+  document.getElementById("root")
 );
