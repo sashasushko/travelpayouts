347d2aa5d5b17932a39033cad327ea8224abd54e
Sasha Sushko <sushko@outlook.com>
2019-03-19 00:05:06 +0500

    Импортирует типы из Реакта

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import Api from './api/api';
 import { ApiProvider } from './api/api-injection';
 import App from './App';
 import './index.css';
+import * as React from "react";
 
 const api = new Api();
 
