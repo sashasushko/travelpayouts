a2f4524a0358abfd1e8e87a1072cfa1941b9f6d1
Sasha Sushko <sushko@outlook.com>
2019-03-19 00:05:48 +0500

    Исправит по стилю Преттиера

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import Api from './api/api';
-import { ApiProvider } from './api/api-injection';
-import App from './App';
-import './index.css';
 import * as React from "react";
 import { render } from "react-dom";
+import Api from "./api/api";
+import { ApiProvider } from "./api/api-injection";
+import App from "./App";
+import "./index.css";
 
 const api = new Api();
 
