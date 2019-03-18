724c44640991d4b0e7c30d610d00a6d514b3596e
Sasha Sushko <sushko@outlook.com>
2019-03-19 00:00:49 +0500

    Создаст компонент для копирайта

---
+++ src/components/Copyright/Copyright.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "./Copyright.css";
+
+function Copyright() {
+  return <p className="copyright">© Travelpayouts, 2011–2018</p>;
+}
+
+export { Copyright as default };
